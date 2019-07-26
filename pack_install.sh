c07cbacd94e654c480a393767cfd58ec3d61cd5b
Anurag Verma <43913907+ultrahacx@users.noreply.github.com>
2019-07-26 17:17:11 +0530

    Add files via upload

---
+++ pack_install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Getting ready to install "
+echo " "
+echo "Check your internet connection before installation"
+echo " "
+echo "Installing GEANY IDE..."
+apt install geany
+echo " "
+echo " "
+echo "Installing GIMP"
+apt install gimp 
+echo " "
+echo "Process completed"
